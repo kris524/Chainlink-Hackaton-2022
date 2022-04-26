c0d8385a5dc9477681a9908439079837f7f75a88
Peter <sqhy2@cam.ac.uk>
2022-04-26 16:10:41 +0100

    add app

--- solana-trading-app/programs/solana-trading-app/src/lib.rs
+++ solana-trading-app/programs/solana-trading-app/src/lib.rs
@@ -1,12 +1,18 @@
-use anchor_lang::prelude::*;
+// use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+// declare_id!("BiHcQCAxV75ziYyyBJmvsK8EjQfEPNBXPoyWfXB8rBuU");
+
+// #[program]
+// mod mysolanaapp {
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+
+declare_id!("BiHcQCAxV75ziYyyBJmvsK8EjQfEPNBXPoyWfXB8rBuU");
 
 #[program]
 pub mod solana_trading_app {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
         Ok(())
     }
 }
