b8bcca89bd32579b1f5f5f0c73cf6d85fb1420e3
Peter <sqhy2@cam.ac.uk>
2022-04-28 23:19:49 +0100

    update dependency

--- solana-trading-app/programs/solana-trading-app/src/lib.rs
+++ solana-trading-app/programs/solana-trading-app/src/lib.rs
@@ -1,15 +1,60 @@
+// use anchor_lang::prelude::*;
+// use anchor_lang::solana_program::entrypoint::ProgramResult;
+
+// declare_id!("BiHcQCAxV75ziYyyBJmvsK8EjQfEPNBXPoyWfXB8rBuU");
+
+// #[program]
+// pub mod solana_trading_app {
+//     use super::*;
+//     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+//         Ok(())
+//     }
+// }
+
+// #[derive(Accounts)]
+// pub struct Initialize {}
+
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("BiHcQCAxV75ziYyyBJmvsK8EjQfEPNBXPoyWfXB8rBuU");
+declare_id!("Aj8Lne9J5iotJxRMJuL3ATowQwqwh9ZqAWr44wJkRSRY");
 
 #[program]
-pub mod solana_trading_app {
+mod solana_trading_app {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn create(ctx: Context<Create>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
         Ok(())
     }
 }
 
+// Transaction instructions
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Transaction instructions
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// An account that goes inside a transaction instruction
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
--- solana-trading-app/tests/solana-trading-app.ts
+++ solana-trading-app/tests/solana-trading-app.ts
@@ -1,30 +1,31 @@
-// import * as anchor from "@project-serum/anchor";
-// import { Program } from "@project-serum/anchor";
-// import { SolanaTradingApp } from "../target/types/solana_trading_app";
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { SolanaTradingApp } from "../target/types/solana_trading_app";
 
-// describe("solana-trading-app", () => {
+describe("solana-trading-app", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.SolanaTradingApp as Program<SolanaTradingApp>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
+
+
+// const anchor = require('@project-serum/anchor');
+
+// describe('solana-trading-app', () => {
 //   // Configure the client to use the local cluster.
-//   anchor.setProvider(anchor.AnchorProvider.env());
+//   anchor.setProvider(anchor.Provider.env());
 
-//   const program = anchor.workspace.SolanaTradingApp as Program<SolanaTradingApp>;
-
-//   it("Is initialized!", async () => {
-//     // Add your test here.
-//     const tx = await program.methods.initialize().rpc();
+//   it('Is initialized!', async () => {
+//     const program = anchor.workspace.SolanaTradingApp;
+//     const tx = await program.rpc.initialize();
 //     console.log("Your transaction signature", tx);
 //   });
 // });
 
-
-const anchor = require('@project-serum/anchor');
-
-describe('solana-trading-app', () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
-
-  it('Is initialized!', async () => {
-    const program = anchor.workspace.SolanaTradingApp;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
-});
